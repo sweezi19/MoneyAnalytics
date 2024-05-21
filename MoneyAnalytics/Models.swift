6cc49b4d256d6ac2c6aa35e620e21a7af65f545f
Theo Tar <sweezi19@gmail.com>
2024-05-22 01:18:26 +0300

    added currencies, new menu

---
+++ MoneyAnalytics/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  MoneyAnalytics
+//
+//  Created by Theo Tar on 21/05/2024.
+//
+
+import Foundation
---
+++ MoneyAnalytics/Сurrencies.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  MoneyAnalytics
+//
+//  Created by Theo Tar on 22/05/2024.
+//
+
+import Foundation
